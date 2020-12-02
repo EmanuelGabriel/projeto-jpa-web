264d39fc7c513c819cf902e0097d42511ab2ce4c
Samuel Gabriel <emanuel.gabriel.sousa@protonmail.com>
2020-12-02 16:00:18 -0300

    criação de testes da classe Fabricante

---
+++ src/main/java/br/com/emanuelgabriel/repository/FabricanteRepository.java
@@ -0,0 +1,8 @@
+package br.com.emanuelgabriel.repository;
+
+import br.com.emanuelgabriel.model.Fabricante;
+import br.com.emanuelgabriel.service.GenericService;
+
+public interface FabricanteRepository extends GenericService<Fabricante> {
+
+}
---
+++ src/test/java/br/com/emanuelgabriel/testes/fabricante/TesteFabricante.java
@@ -0,0 +1,23 @@
+package br.com.emanuelgabriel.testes.fabricante;
+
+import org.junit.Test;
+
+public class TesteFabricante {
+
+	private static final String FABRICANTE_NAO_ENCONTRADO = "Fabricante não encontrado";
+	private static final String NENHUM_REGISTRO_ENCONTRADO = "Nenhum registro encontrado";
+
+	public TesteFabricante() {
+	}
+
+	@Test
+	public void salvar() {
+
+	}
+
+	@Test
+	public void findAll() {
+
+	}
+
+}
