3385e49a5c388d9dd04f93b985370b3dc3df2751
Samuel Gabriel <emanuel.gabriel.sousa@protonmail.com>
2020-12-02 15:52:21 -0300

    criação da entidade Fabricante

---
+++ src/main/java/br/com/emanuelgabriel/model/enums/TipoCategoria.java
@@ -0,0 +1,14 @@
+package br.com.emanuelgabriel.model.enums;
+
+public enum TipoCategoria {
+	
+	HATCH_COMPACTO,
+	HATCH_MEDIO,
+	SEDAN_COMPACTO,
+	SEDAN_MEDIO,
+	SEDAN_GRANDE,
+	MINIVAN,
+	ESPORTIVO,
+	UTILITARIO_COMERCIAL
+
+}
