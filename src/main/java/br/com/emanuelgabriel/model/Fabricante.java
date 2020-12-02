2ea4f6f266056eb583f587cd7a7f8d3be7daca3e
Samuel Gabriel <emanuel.gabriel.sousa@protonmail.com>
2020-12-02 15:54:54 -0300

    criando os atributos da entidade Fabricante

--- src/main/java/br/com/emanuelgabriel/model/Fabricante.java
+++ src/main/java/br/com/emanuelgabriel/model/Fabricante.java
@@ -16,5 +16,69 @@ public class Fabricante implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+	private String nome;
+	private String descricao;
+
+	public Fabricante() {
+	}
+
+	public Fabricante(String nome, String descricao) {
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Fabricante other = (Fabricante) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Fabricante [codigo=" + codigo + ", nome=" + nome + ", descricao=" + descricao + "]";
+	}
 
 }
